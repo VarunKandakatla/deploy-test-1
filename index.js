4e846fa79620a9427145323081767010aba39688
Varun Kandakatla <varunkumarkandakatla@gmail.com>
2024-04-26 18:51:50 +0530

    added files

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 app.listen(port);
 app.use(express.json());
 
+let dataList = [];
+
+get().then(()=>{
+    console.log('loaded...!');
+})
+
 app.get('/',(req,res)=>{
     res.status(200).json({message : "Hello from express!"});
 })
 
-app.get('/:name',(req,res)=>{
-    res.status(200).json({message : `Hello ${req.params.name}! How are you?`});
+app.get('/add/:name',(req,res)=>{
+    const name = req.params.name;
+    dataList.push(name);
+    save().then(()=>{
+        res.status(200).json({message : "saved!"});
+    })
 })
+
+app.get('/all',(req,res)=>{
+    res.status(200).json({Data : dataList});
+})
+
+app.get('*',(req,res)=>{
+    res.send("Invalid Route");
+})
+
+function save()
+{
+    return new Promise((resolve,reject)=>{
+        fs.writeFile('file.json',JSON.stringify(dataList),(err)=>{
+            resolve();
+        })
+    })
+}
+
+function get()
+{
+    return new Promise((resolve,reject)=>{
+        fs.readFile('file.json','utf-8',(err,data)=>{
+            dataList = JSON.parse(data);
+            resolve();
+        })
+    })
+}
